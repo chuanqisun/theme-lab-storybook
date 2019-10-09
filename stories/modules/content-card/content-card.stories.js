e9da776a036b2569f8fe4af32b35d96a6190be89
Chuánqí Sun <chusun@microsoft.com>
2019-10-09 11:26:20 -0700

    rename files

--- stories/modules/content-card/content-card.stories.js
+++ stories/modules/content-card/content-card.stories.js
@@ -7,42 +7,42 @@ export default {
 };
 
 export const CardImg = () => html`
-  <img class="content-card__img" src="https://via.placeholder.com/800X600" />
+  <img class="m-content-card__img" src="https://via.placeholder.com/800X600" />
 `;
 export const CardHeading = () => html`
-  <h2 class="content-card__heading">Card heading</h2>
+  <h2 class="m-content-card__heading">Card heading</h2>
 `;
 export const CardParagraph = () => html`
-  <p class="content-card__paragraph">
+  <p class="m-content-card__paragraph">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
   </p>
 `;
 export const CardAction = () => html`
-  <a href="#" class="content-card__action">Call to action</a>
+  <a href="#" class="m-content-card__action">Call to action</a>
 `;
 
 export const ContentCardCardLayout = () => html`
-  <div class="content-card content-card--limit-width">
+  <div class="m-content-card m-content-card--limit-width">
     ${CardImg()}
-    <div class="content-card__text">
+    <div class="m-content-card__text">
       ${CardHeading()} ${CardParagraph()} ${CardAction()}
     </div>
   </div>
 `;
 
 export const ContentCardHeroLayout = () => html`
-  <div class="content-card content-card--text-over">
+  <div class="m-content-card m-content-card--text-over">
     ${CardImg()}
-    <div class="content-card__text content-card__text--with-padding content-card__text--align-center">
+    <div class="m-content-card__text m-content-card__text--with-padding m-content-card__text--align-center">
       ${CardHeading()} ${CardParagraph()} ${CardAction()}
     </div>
   </div>
 `;
 
 export const ContentCardFeatureLayout = () => html`
-  <div class="content-card content-card--text-aside">
+  <div class="m-content-card m-content-card--text-aside">
     ${CardImg()}
-    <div class="content-card__text content-card__text--with-padding">
+    <div class="m-content-card__text m-content-card__text--with-padding">
       ${CardHeading()} ${CardParagraph()} ${CardAction()}
     </div>
   </div>
--- stories/modules/product-card/product-card.stories.js
+++ stories/modules/product-card/product-card.stories.js
@@ -5,40 +5,47 @@ export default {
   title: 'Modules/Product card',
 };
 
-const img = html`<img class="product-card__img" src="https://via.placeholder.com/200X350" />`;
-const heading = html`<h2 class="product-card__heading">Card heading</h2>`;
-const paragraph = html`<p class="product-card__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>`;
-const rating = html`<div>****-</div>`;
-const price = html`<div>$99.99</div>`;
+const img = html`
+  <img class="m-product-card__img" src="https://via.placeholder.com/200X350" />
+`;
+const heading = html`
+  <h2 class="m-product-card__heading">Card heading</h2>
+`;
+const paragraph = html`
+  <p class="m-product-card__paragraph">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p>
+`;
+const rating = html`
+  <div>****-</div>
+`;
+const price = html`
+  <div>$99.99</div>
+`;
 
 export const basic = storyFrom(html`
-  <a href='#' class="product-card product-card--small">
+  <a href="#" class="m-product-card m-product-card--small">
     ${img}
-    <div class="product-card__text">
-      ${heading}
-      ${price}
+    <div class="m-product-card__text">
+      ${heading} ${price}
     </div>
   </a>
 `);
 
 export const withDetails = storyFrom(html`
-  <a href="#" class="product-card product-card--small">
+  <a href="#" class="m-product-card m-product-card--small">
     ${img}
-    <div class="product-card__text">
-      ${heading}
-      ${paragraph}
-      ${price}
+    <div class="m-product-card__text">
+      ${heading} ${paragraph} ${price}
     </div>
   </a>
 `);
 
 export const withRating = storyFrom(html`
-<a href="#" class="product-card product-card--small">
-  ${img}
-  <div class="product-card__text">
-    ${heading}
-    ${rating}
-    ${price}
-  </div>
-</a>
+  <a href="#" class="m-product-card m-product-card--small">
+    ${img}
+    <div class="m-product-card__text">
+      ${heading} ${rating} ${price}
+    </div>
+  </a>
 `);
