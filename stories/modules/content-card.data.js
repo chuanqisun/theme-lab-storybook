9f61787963dd6cf859ae460734da0f74b9843571
Chuanqi <henrysun918@gmail.com>
2019-10-09 22:46:53 -0700

    finish content-card stories

---
+++ stories/modules/content-card.data.js
@@ -0,0 +1,36 @@
+export const contentCardDefaultData = {
+  image: {
+    src: 'https://via.placeholder.com/400X400',
+  },
+  heading: 'Headline',
+  paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  actions: ['Shop now'],
+};
+
+export const contentCardHalfCardHalfData = {
+  ...contentCardDefaultData,
+  image: {
+    src: 'https://via.placeholder.com/548X468',
+  },
+};
+
+export const contentCardThirdCardData = {
+  ...contentCardDefaultData,
+  image: {
+    src: 'https://via.placeholder.com/352X432',
+  },
+};
+
+export const contentCardFeatureData = {
+  ...contentCardDefaultData,
+  image: {
+    src: 'https://via.placeholder.com/728X432',
+  },
+};
+
+export const contentCardHeroData = {
+  ...contentCardDefaultData,
+  image: {
+    src: 'https://via.placeholder.com/1440X500',
+  },
+};
--- stories/modules/content-card.stories.js
+++ stories/modules/content-card.stories.js
@@ -1,12 +1,13 @@
 import { html, storyFrom } from '../utils/story-from';
+import { contentCardHeroData, contentCardHalfCardHalfData, contentCardThirdCardData, contentCardFeatureData } from './content-card.data';
 import '../styles/index.scss';
 
 export default {
   title: 'Modules/Content card',
-  includeStories: ['cardLayout', 'featureLayout', 'heroLayout'],
+  includeStories: ['card', 'feature', 'hero'],
 };
 
-export const CardImg = ({ dimension = '1440X500' } = {}) => html`
+export const CardImg = ({ dimension } = { dimension: '1440X500' }) => html`
   <img class="m-content-card__img" src="https://via.placeholder.com/${dimension}" />
 `;
 export const CardHeading = () => html`
@@ -23,35 +24,95 @@ export const CardActions = () => html`
   </div>
 `;
 
-export const ContentCardCardLayout = () => html`
+export const ContentCardCardLayout = (data = contentCardHalfCardHalfData) => html`
   <div class="m-content-card">
-    ${CardImg({ dimension: '400X400' })}
-    <div class="m-content-card__text">
-      ${CardHeading()} ${CardParagraph()} ${CardActions()}
+    <img class="img" src="${data.image.src}" />
+    <div class="text-container">
+      <h2 class="heading">${data.heading}</h2>
+      <p class="paragraph">${data.paragraph}</p>
+      <div class="actions">
+        ${data.actions.map(
+          action =>
+            html`
+              <a href="#">${action}</a>
+            `
+        )}
+      </div>
     </div>
   </div>
 `;
 
-export const ContentCardHeroLayout = () => html`
-  <div class="m-content-card m-content-card--text-over">
-    ${CardImg()}
-    <div class="m-content-card__text m-content-card__text--with-padding m-content-card__text--align-center">
-      ${CardHeading()} ${CardParagraph()} ${CardActions()}
+export const ContentCardFeatureTextRight = (data = contentCardFeatureData) => {
+  return html`
+    <div class="m-content-card text-right">
+      <img class="img" src="${data.image.src}" />
+      <div class="text-container">
+        <h2 class="heading">${data.heading}</h2>
+        <p class="paragraph">${data.paragraph}</p>
+        <div class="actions">
+          ${data.actions.map(
+            action => html`
+              <a href="#">${action}</a>
+            `
+          )}
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+export const ContentCardFeatureTextLeft = (data = contentCardFeatureData) => {
+  return html`
+    <div class="m-content-card text-left">
+      <img class="img" src="${data.image.src}" />
+      <div class="text-container">
+        <h2 class="heading">${data.heading}</h2>
+        <p class="paragraph">${data.paragraph}</p>
+        <div class="actions">
+          ${data.actions.map(
+            action => html`
+              <a href="#">${action}</a>
+            `
+          )}
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+export const ContentCardHeroLayout = (data = contentCardHeroData) => html`
+  <div class="m-content-card hero">
+    <img class="img" src="${data.image.src}" />
+    <div class="text-container">
+      <h2 class="heading">${data.heading}</h2>
+      <p class="paragraph">${data.paragraph}</p>
+      <div class="actions">
+        ${data.actions.map(
+          action => html`
+            <a href="#">${action}</a>
+          `
+        )}
+      </div>
     </div>
   </div>
 `;
 
-export const ContentCardFeatureLayout = () => html`
-  <div class="m-content-card m-content-card--text-aside">
-    ${CardImg()}
-    <div class="m-content-card__text m-content-card__text--with-padding">
-      ${CardHeading()} ${CardParagraph()} ${CardActions()}
-    </div>
+export const card = storyFrom(html`
+  <div class="demo-notes">
+    1. This is showing multiple instances of the same module.<br />
+    2. The cards should reflow in narrow viewport. The reflow behavior should be handled by the multi-column container and is NOT part of this demo.<br />
+    3. Never use these modules in full-bleed layout because the texts have no side padding. They will touch the edge of screen when in full-bleed.
   </div>
-`;
+  <div class="u-columns">
+    ${ContentCardCardLayout()} ${ContentCardCardLayout()}
+  </div>
+  <div class="u-columns">
+    ${ContentCardCardLayout(contentCardThirdCardData)} ${ContentCardCardLayout(contentCardThirdCardData)} ${ContentCardCardLayout(contentCardThirdCardData)}
+  </div>
+`);
 
-export const cardLayout = storyFrom(ContentCardCardLayout());
+export const feature = storyFrom(html`
+  ${ContentCardFeatureTextRight()} ${ContentCardFeatureTextLeft()}
+`);
 
-export const featureLayout = storyFrom(ContentCardFeatureLayout());
-
-export const heroLayout = storyFrom(ContentCardHeroLayout());
+export const hero = storyFrom(ContentCardHeroLayout());
