47f1279a75de5615816544ba656b5c70310a9f8f
Chuanqi <henrysun918@gmail.com>
2019-10-12 23:53:20 -0700

    add lost grid

--- react-lab/.storybook/webpack.config.js
+++ react-lab/.storybook/webpack.config.js
@@ -3,7 +3,20 @@ const path = require('path');
 module.exports = async ({ config, mode }) => {
   config.module.rules.push({
     test: /\.scss$/,
-    use: ['style-loader', 'css-loader', 'sass-loader'],
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+          config: {
+            path: '.storybook/postcss.config.js',
+          },
+        },
+      },
+      'sass-loader',
+    ],
     include: path.resolve(__dirname, '../'),
   });
 
