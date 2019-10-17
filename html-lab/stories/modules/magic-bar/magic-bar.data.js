77e93a19b0d93cd3751e42a2206365bc7524e69a
Chuánqí Sun <chusun@microsoft.com>
2019-10-17 12:33:46 -0700

    move all files into root level and remove html lab

--- html-lab/stories/modules/magic-bar/magic-bar.data.js
+++
@@ -1,3 +0,0 @@
-import { boolean, number, text, withKnobs } from '@storybook/addon-knobs';
-
-export const message = () => text('message', 'default test');
--- html-lab/stories/modules/message-bar/message-bar.data.js
+++
@@ -1,7 +0,0 @@
-export const messageBarDefaultData = {
-  message: 'Summer sale all items 15% off',
-};
-
-export const messageBarMultiMessageData = {
-  messages: ['Message 1 of 3', 'Message 2 of 3', 'Message 3 of 3'],
-};
