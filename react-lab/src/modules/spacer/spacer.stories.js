f6048372750e26fa73451ef9fe8768088dd4953f
Chuánqí Sun <chusun@microsoft.com>
2019-10-16 20:39:09 -0700

    update spacer demo

--- react-lab/src/modules/spacer/spacer.stories.js
+++ react-lab/src/modules/spacer/spacer.stories.js
@@ -5,6 +5,9 @@ import notes from './spacer.notes.md';
 
 export default { title: 'Modules/Spacer', decorators: [withKnobs], parameters: { notes } };
 
+const sampleText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
 export const basic = () => (
   <>
     <style
@@ -16,12 +19,8 @@ export const basic = () => (
     `,
       }}
     />
+    <div>{sampleText}</div>
     <Spacer />
-    <br />
-    <Spacer />
-    <br />
-    <Spacer />
-    <br />
-    <Spacer />
+    <div>{sampleText}</div>
   </>
 );
