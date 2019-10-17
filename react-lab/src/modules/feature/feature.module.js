e044a76ea44b264fe3e0876400bd8afeebbb2318
Chuánqí Sun <chusun@microsoft.com>
2019-10-16 20:31:22 -0700

    switch to native css grid

--- react-lab/src/modules/feature/feature.module.js
+++ react-lab/src/modules/feature/feature.module.js
@@ -10,12 +10,15 @@ export const Feature = ({
   ...props
 }) => (
   <div className={`m-feature ${layout}`}>
-    <picture>
-      {image.sources.map((source, i) => (
-        <source key={i} media={source.media} srcSet={source.srcSet} />
-      ))}
-      <img className="img" src={image.src} srcSet={image.srcSet} />
-    </picture>
+    {/* using the image-container to allow IE11 CSS grid to correctly position the image */}
+    <div className="image-container">
+      <picture>
+        {image.sources.map((source, i) => (
+          <source key={i} media={source.media} srcSet={source.srcSet} />
+        ))}
+        <img className="img" src={image.src} srcSet={image.srcSet} />
+      </picture>
+    </div>
     <div className="text-container">
       {heading && <h2 className="heading">{heading}</h2>}
       {body && <p className="body">{body}</p>}
