5b1022dceccfb858945336a2507dc49e821085e4
Chuánqí Sun <chusun@microsoft.com>
2019-10-21 17:55:30 -0700

    add text input

---
+++ client/src/components/text-input/text-input.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import notes from './text-input.notes.md';
+
+export default { title: 'Components/Text input', parameters: { notes } };
+
+export const basic = () => <input className="c-text-input" type="text" />;
