3289d6d12fd7b914ede74be47f39a352e5aeb44d
Chuánqí Sun <chusun@microsoft.com>
2019-11-13 09:20:30 -0800

    add menu html

---
+++ client/src/modules/header/components/categories.component.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Categories = props => (
+  <div className="mc-categories">
+    <ul>
+      <li>
+        <a href="javascript:void(0)">Men</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Women</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Kids</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Home</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Beauty</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Gifts</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Sale</a>
+      </li>
+    </ul>
+  </div>
+);
--- client/src/modules/header/header.stories.js
+++ client/src/modules/header/header.stories.js
@@ -3,6 +3,7 @@ import React from "react";
 import { headerData, headerDataSignedIn, knobify } from "./header.data";
 import { Header } from "./header.module";
 import { BasicMenu } from "./components/basic-menu.component";
+import { Categories } from "./components/categories.component";
 import notes from "./header.notes.md";
 
 export default {
@@ -14,3 +15,4 @@ export default {
 export const basic = () => <Header {...knobify(headerData)} />;
 export const signedIn = () => <Header {...knobify(headerDataSignedIn)} />;
 export const desktopBasicMenu = () => <BasicMenu />;
+export const desktopCategories = () => <Categories />;
