9707e18d4b19d0fdc155d7a641ab1727a54e2ba9
Chuánqí Sun <chusun@microsoft.com>
2019-11-12 16:51:50 -0800

    add desktop menu styles

---
+++ client/src/modules/header/components/basic-menu.component.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const BasicMenu = props => (
+  <div className="mc-dropdown-menu">
+    <ul>
+      <li>
+        <a href="javascript:void(0)">Featured</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Clothing</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Shoes</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Accessories</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Jewelry</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Featured category</a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">Shop by brand</a>
+      </li>
+    </ul>
+  </div>
+);
--- client/src/modules/header/header.stories.js
+++ client/src/modules/header/header.stories.js
@@ -1,10 +1,16 @@
-import { withKnobs } from '@storybook/addon-knobs';
-import React from 'react';
-import { headerData, headerDataSignedIn, knobify } from './header.data';
-import { Header } from './header.module';
-import notes from './header.notes.md';
+import { withKnobs } from "@storybook/addon-knobs";
+import React from "react";
+import { headerData, headerDataSignedIn, knobify } from "./header.data";
+import { Header } from "./header.module";
+import { BasicMenu } from "./components/basic-menu.component";
+import notes from "./header.notes.md";
 
-export default { title: 'Modules/Header', decorators: [withKnobs], parameters: { notes } };
+export default {
+  title: "Modules/Header",
+  decorators: [withKnobs],
+  parameters: { notes }
+};
 
 export const basic = () => <Header {...knobify(headerData)} />;
 export const signedIn = () => <Header {...knobify(headerDataSignedIn)} />;
+export const desktopBasicMenu = () => <BasicMenu />;
