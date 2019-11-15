92deee975f9e397f14ba116710aa19a602b35f61
Chuánqí Sun <chusun@microsoft.com>
2019-11-15 15:30:01 -0800

    add input styles

--- client/src/components/input/input.stories.js
+++ client/src/components/input/input.stories.js
@@ -4,9 +4,28 @@ import notes from "./input.notes.md";
 export default { title: "Components/Input", parameters: { notes } };
 
 export const textInput = () => <input className="c-input" type="text" />;
+
 export const textInputWithLabel = () => (
-  <div>
-    <label htmlFor="addr-1">Address line 1</label>
+  <div className="c-field">
+    <label className="c-label" htmlFor="addr-1">
+      <span className="c-label__content">Address line 1</span>
+    </label>
     <input id="addr-1" name="addr-1" className="c-input" type="text" />
+  </div>
+);
+
+export const textInputWithError = () => (
+  <div className="c-field">
+    <label className="c-label" htmlFor="addr-1">
+      <span className="c-label__content">Address line 1</span>
+      <span className="c-label__error">Please provide a valid addres</span>
+    </label>
+    <input
+      id="addr-1"
+      name="addr-1"
+      className="c-input"
+      type="text"
+      invalid=""
+    />
   </div>
 );
